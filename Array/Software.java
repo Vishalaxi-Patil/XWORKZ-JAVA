041584735b184614145cf86c2d9f6486f4413419
Vishalaxi-Patil <72905004+Vishalaxi-Patil@users.noreply.github.com>
2020-10-22 12:57:20 +0530

    Accesing index value

---
+++ Array/Software.java
@@ -0,0 +1,14 @@
+public class Software
+{
+	public static void main(String[] trainees)
+	{
+		System.out.println("JVM invoked main method");
+		System.out.println("Software trainees" +trainees);
+		int size=trainees.length;
+		System.out.println("size of param=" +size);
+		System.out.println("Element @ index 0 "+trainees[0]);
+	    System.out.println("Element @ index 1 "+trainees[1]);
+	    System.out.println("exit main");
+	}
+}
+
