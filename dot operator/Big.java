8706e040b2565e15e65824e850f7164763663ae5
Vishalaxi-Patil <72905004+Vishalaxi-Patil@users.noreply.github.com>
2020-10-23 21:57:34 +0530

    giving value at run time

---
+++ dot operator/Big.java
@@ -0,0 +1,11 @@
+public class Big
+{
+	public static void trainee(String[] size)
+	{
+		System.out.println("Size of photo" +size);
+		int sizeOfArray=size.length;
+		System.out.println("size of an array" +sizeOfArray);
+		System.out.println("element at index zero" +size[0]);
+		System.out.println("element at index one" +size[1]);
+	}
+}
