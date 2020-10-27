9306f59ad91f7e2ec7671398cb3930994056f1a7
Vishalaxi-Patil <72905004+Vishalaxi-Patil@users.noreply.github.com>
2020-10-27 22:49:38 +0530

    Add files via upload

---
+++ return-type/AcademicResult.java
@@ -0,0 +1,10 @@
+public class AcademicResult
+{
+	public static void main(String[] result)
+	{
+		System.out.println("JVM invoked main");
+		 String resultInPercentage=Percentage.report(70);
+		 System.out.println("Return value from reprt=" +resultInPercentage);
+		 
+	}
+}
